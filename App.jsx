261b5d230aa560dd314bd59f2bff31473405d7b2
Md-hasrat <hasrat233222@gmail.com>
2025-02-18 22:53:53 +0530

    First react app

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style= {{fontSize: 40}}>Mobile Development with React Native</Text>
+      <Text style={{fontSize: 35}}>Mobile Development with React Native</Text>
+      <Button title='Press here' /> 
+    </View>
+  )
+}
+
+export default App
